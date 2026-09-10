dafdb7b0dcc0c635d18ada8117771b4519b8de99
Jay A. <jay.m.appleton@gmail.com>
2026-09-10 14:37:20 +0200

    needed to set myst parser and point to index.md

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+project = "Granular RVEs"
+author = "Jay M. Appleton"
+
+extensions = [
+    "myst_parser",
+]
+
+source_suffix = {
+    ".md": "markdown",
+    ".rst": "restructuredtext",
+}
+
+master_doc = "index"
+
+html_theme = "pydata_sphinx_theme"
+
+html_title = "Granular RVEs"
